12fe64feb46c3fbbf7f16128d14135b815374bcc
MJHyuk <shekwl24@gmail.com>
2020-04-15 18:56:36 +0900

    자바스크립트 변수명 캐멀표기법으로 변경 모바일 기기 최적화

--- resultFn.js
+++ resultFn.js
@@ -4,7 +4,7 @@ function smeyrMesoCostS(n) {
   const d=7130000;
   return (n * (2 * a + ((n - 1) * d))) / 2;
 }
-function etc_MesoCostS(n) {
+function etcMesoCostS(n) {
   const a=19040000;
   const d=6600000;
   return (n * (2 * a + ((n - 1) * d))) / 2;
@@ -14,32 +14,32 @@ function symbolNumberS(n) {
 }
 
 // 만렙까지 필요한 메소 계산
-function smeyr_calMeso(level) {
+function smeyrCalMeso(level) {
   if(level == 1) return smeyrMesoCostS(19);
   else return smeyrMesoCostS(19) - smeyrMesoCostS(level - 1);
 }
-function etc_calMeso(level) {
-  if(level == 1) return etc_MesoCostS(19);
-  else return etc_MesoCostS(19) - etc_MesoCostS(level - 1);
+function etcCalMeso(level) {
+  if(level == 1) return etcMesoCostS(19);
+  else return etcMesoCostS(19) - etcMesoCostS(level - 1);
 }
 
 // 만렙까지 필요한 심볼 개수 계산
-function required_symbol(level, number) {
+function requiredSymbol(level, number) {
   if(level == 1) return symbolNumberS(19) - number;
   else return symbolNumberS(19) - symbolNumberS(level - 1) - number;
 }
 
 // 만렙 달성 날짜 계산
-function calDate(level, number, symbol_perDay) {
+function calDate(level, number, symbolPerDay) {
     let today = new Date();
-    let required_day = Math.ceil(required_symbol(level, number) / symbol_perDay);
+    let requiredDay = Math.ceil(requiredSymbol(level, number) / symbolPerDay);
 
-    today.setDate(today.getDate() + required_day);
+    today.setDate(today.getDate() + requiredDay);
     let year = today.getFullYear(); // 년도
     let month = today.getMonth() + 1;  // 월
     let date = today.getDate();  // 날짜
     let day = today.getDay();  // 요일
-    let maxLevelDate = year + '년' + month + '월' + date + '일' + ' (' + required_day + '일)';
+    let maxLevelDate = year + '년' + month + '월' + date + '일' + '<br />' + '(' + requiredDay + '일)';
 
     return maxLevelDate;
 }
