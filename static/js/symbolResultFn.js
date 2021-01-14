1243a8e38746f494997fe28104e3628977d71690
MJHyuk <shekwl24@gmail.com>
2021-01-14 15:28:29 +0900

    어센틱 심볼 추가

--- static/js/symbolResultFn.js
+++ static/js/symbolResultFn.js
@@ -5,6 +5,12 @@ function smeyrMesoCostS(n) {
     return (n * (2 * a + ((n - 1) * d))) / 2;
 }
 
+function athenMesoCostS(n) {
+    const a = 185400000;
+    const d = 88500000;
+    return (n * (2 * a + ((n - 1) * d))) / 2;
+}
+
 function etcMesoCostS(n) {
     const a = 19040000;
     const d = 6600000;
@@ -15,10 +21,19 @@ function symbolNumberS(n) {
     return ((n * (n + 1) * (2 * n + 1)) / 6) + 11 * n;
 }
 
+function athenSymbolNumberS(n) {
+    return ((9 * n * (n + 1) * (2 * n + 1)) / 6) + 10 * n * (n + 1);
+}
+
 // 만렙까지 필요한 메소 계산
 function smeyrCalMeso(level) {
     if (level == 1) return smeyrMesoCostS(19);
     else return smeyrMesoCostS(19) - smeyrMesoCostS(level - 1);
+}
+
+function athenCalMeso(level) {
+    if (level == 1) return athenMesoCostS(10);
+    else return athenMesoCostS(10) - athenMesoCostS(level - 1);
 }
 
 function etcCalMeso(level) {
@@ -32,10 +47,29 @@ function requiredSymbol(level, number) {
     else return symbolNumberS(19) - symbolNumberS(level - 1) - number;
 }
 
+function athenRequiredSymbol(level, number) {
+    if (level == 1) return athenSymbolNumberS(10) - number;
+    else return athenSymbolNumberS(10) - athenSymbolNumberS(level - 1) - number;
+}
+
 // 만렙 달성 날짜 계산
 function calDate(level, number, symbolPerDay) {
     let today = new Date();
     let requiredDay = Math.ceil(requiredSymbol(level, number) / symbolPerDay);
+
+    today.setDate(today.getDate() + requiredDay);
+    let year = today.getFullYear(); // 년도
+    let month = today.getMonth() + 1; // 월
+    let date = today.getDate(); // 날짜
+    let day = today.getDay(); // 요일
+    let maxLevelDate = year + '년' + month + '월' + date + '일' + '<br />' + '(' + requiredDay + '일)';
+
+    return maxLevelDate;
+}
+
+function athenCalDate(level, number, symbolPerDay) {
+    let today = new Date();
+    let requiredDay = Math.ceil(athenRequiredSymbol(level, number) / symbolPerDay);
 
     today.setDate(today.getDate() + requiredDay);
     let year = today.getFullYear(); // 년도
